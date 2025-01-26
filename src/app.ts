c6a659c809e5e1a256523444443c11f12c80103a
Md Abdul Adud <sopnilali24@gmail.com>
2025-01-26 22:40:29 +0600

    production live url add

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json())
 // CORS
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Allow requests from this specific origi,
+    origin: ['http://localhost:5173', 'https://stationery-shop-client-six.vercel.app'], // Allow requests from this specific origi,
     credentials: true 
 },
 ));
